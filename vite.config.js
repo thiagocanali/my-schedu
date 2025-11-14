436c71cdf497784518e9016e9be4adb294021f41
Thiago Schwartz <67984677+thiagocanali@users.noreply.github.com>
2025-11-14 12:09:12 -0300

    ajusta base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: '/my-schedu/',  // <— aqui! use exatamente o nome do repo
   plugins: [vue()],
   resolve: {
     alias: {
